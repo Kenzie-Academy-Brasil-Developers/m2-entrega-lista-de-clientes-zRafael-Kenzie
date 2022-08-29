70570a514c37154fdbc254fa87333e84b3f68c3d
zRafael <rafael.soaress012@gmail.com>
2022-08-29 16:41:48 -0300

    Feat:lista de clientes agora lista os clientes

--- src/js/api.js
+++ src/js/api.js
@@ -1,21 +1,73 @@
-class Api {
-  
-    static async listarClientes(){
+import { Clientes } from "./listar.js"
+
+export class Api {
+
+    static baseUrl = 'https://atividade-api-clientes.herokuapp.com/clientes'
+
+    static async listarClientes() {
+        const data = await fetch(this.baseUrl, {
+            method: "GET",
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        })
+            .then(res => res.json())
+            .then(res =>res)
+            .catch(err => console.log(err))
+
+            Clientes.criarCard(data)
+            return data
 
     }
 
-    static async cadastrarCliente(data){
+    static async cadastrarCliente(data) {
+        const cadastrar = await fetch(this.urlBase, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(data)
+        })
 
+            .then(res => res.json())
+            .catch(err => console.log(err))
+
+        return cadastrar
     }
 
-    static async editarCliente(id, data){
-
+    static async pesquisaCliente (idCliente) {
+        return await fetch(`${this.baseUrl}/${idCliente}`)
+        .then(resp => resp.json())
+        .catch(err => console.log(err))
     }
-
+ 
     static async deletarCliente(id){
+        const deletar = await fetch(`${this.urlBase}${id}`,{
+            method: 'DELETE',
+            headers: {
+                'Content-Type':'application/json'
+        }
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
 
+        return deletar
     }
+
+    static async editarCliente(id, body){
+        const newclient = await fetch(`${this.url}/clientes/${id}`, {
+            method : "PATCH",
+            headers: this.headers,
+            body: JSON.stringify(body)
+        })
+        .then(res => res.json())
+        .catch(err => console.log(err))
+    }
+
 
 }
-  
-export {Api}
+
+Api.listarClientes()
+//Api.cadastrarCliente()
+// Api.editarCliente()
+// Api.deletarCliente()
